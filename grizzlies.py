a0366e988ff6e3a2dad32da45291e333c4b2f472
j4s0np13rc3 <oguzhanakdogan2710@gmail.com>
2021-12-25 22:51:58 +0300

    Added new functions
    Added standard deviation function and extract function. Also pycache file is in .gitignore now.

--- grizzlies.py
+++ grizzlies.py
@@ -24,3 +24,25 @@ def max(str):
             pass
     return reference
 
+def stdev(str):
+    variance = 0
+    total = 0
+    avg = average(str)
+    length = len(str)
+    for entry in str:
+        total = float(pow(avg - entry,2)) + total
+    variance = total/length
+    return math.sqrt(variance)
+
+def extract(str,num,operation):
+    chain = []
+    if operation == 'floor':
+        for entry in str:
+            if  entry >= num:
+                chain.append(entry)
+    elif operation == 'ceil':
+        for entry in str:
+            if  entry < num:
+                chain.append(entry)
+    return chain
+
